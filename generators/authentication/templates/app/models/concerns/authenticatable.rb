8c0ecea5df92d5d90f20d61940a138705f6c8d8c
Lucas Wieloch <lucasrizel@ioasys.com.br>
2016-11-12 17:04:09 -0200

    updated authenticatable model concern

--- generators/authentication/templates/app/models/concerns/authenticatable.rb
+++ generators/authentication/templates/app/models/concerns/authenticatable.rb
@@ -2,14 +2,18 @@ module Authenticatable
 	extend ActiveSupport::Concern
 
 	included do
+		# Associations
 		has_many :authentications, as: :authable, dependent: :destroy
 
+		# Validations
 		validates_presence_of :provider
 		validates :uid, presence: true, uniqueness: true
 
+		# Attribute accessors
 		attr_accessor :created_auth
 	end
 
+	# Class methods
 	module ClassMethods
 
 		def sign_in!(auth_value, password, metadata={})
@@ -39,20 +43,40 @@ module Authenticatable
 		end
 
 		def authenticate_by_token(uid, client, access_token)
-			user = User.find_by_uid(uid)
+			user = find_by_uid(uid)
 			unless user.nil?
 				authentication = user.authentications.find_by_client(client)
-				if !authentication.nil?  and Authentication.token_compare(access_token, authentication.encrypted_access_token)
+				if !authentication.nil? and Authentication.token_compare(access_token, authentication.encrypted_access_token)
 					return user
 				end
 			end
 			return nil
 		end
 
+		def reset_password(email)
+	    resource = find_by_uid!(email)
+			min_length = Devise.password_length.first
+			pass_len = min_length.even? ? min_length/2 : ((min_length/2) + 1)
+			password = SecureRandom.hex(pass_len)
+			resource.update!(password: password)
+	    send_recovery_password_email(resource, password)
+	  end
+
+		private
+
+			def send_recovery_password_email(resource, password)
+				AuthMailer.send_recovery_password_email(resource, password).deliver_now
+			end
+
 	end
 
+	# Instance methods
 	def update_with_password!(params)
 		self.update_with_password(params) || raise(ActiveRecord::RecordInvalid.new(self))
+	end
+
+	def bypass_sign_in(metadata={})
+		self.created_auth = self.authentications.create!(metadata: metadata)
 	end
 
 	def email=(em)
@@ -64,7 +88,7 @@ module Authenticatable
 
 	def sign_out(client)
 		auth  = self.authentications.find_by_client!(client)
-		# raise Authentication::InvalidClient if auth.nil?
+		# raise CustomException::Authentication::InvalidClient if auth.nil?
 		auth.destroy!
 	end
 
